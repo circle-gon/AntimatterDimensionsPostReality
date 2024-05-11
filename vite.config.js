fcca1d59be889e77cf9e172901131236899c4cb7
circle-gon <97845741+circle-gon@users.noreply.github.com>
2024-05-11 16:24:17 -0400

    chores

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
 import { fileURLToPath } from "url";
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-  const STEAM = env.VITE_STEAM === "true";
-  const path = fileURLToPath(new URL("./src", import.meta.url));
-
-  return {
-    plugins: [createVuePlugin()],
-    base: "./",
-    outDir: STEAM ? "../AppFiles" : "dist",
-    resolve: {
-      alias: {
-        "@": path,
-      },
-      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+export default defineConfig({
+  plugins: [createVuePlugin()],
+  base: "./",
+  outDir: "dist",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  };
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
 });
